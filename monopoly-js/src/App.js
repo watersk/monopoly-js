1acd4652f57745a910752d3d504b478d699a70f8
K Waters <waterskm@mail.uc.edu>
2018-08-19 17:59:50 -0500

    material ui stuff needs relearning

--- monopoly-js/src/App.js
+++ monopoly-js/src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state={
+      d1: 1,
+      d2: 1,
+      rollTotal: 2
+    };
+
+    this.handleRoll = this.handleRoll.bind(this);
+  }
+
+  handleRoll(e) {
+    this.state.d1 = Math.floor((Math.random()*6) + 1);
+    this.state.d2 = Math.floor((Math.random()*6) + 1);
+    this.state.rollTotal = this.state.d1 + this.state.d2;
+
+    window.alert("Die 1: " + this.state.d1 + " and Die 2: " + this.state.d2 + " = " + this.state.rollTotal);
+    return false;
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="App-title">Monopoly-JS</h1>
+        <div>
+          <Button onClick={this.handleRoll}>Roll</Button>
+        </div>
       </div>
     );
   }

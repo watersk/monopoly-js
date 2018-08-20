cc7c64e8d7dc2b84820882915185d4db35c46e63
K Waters <waterskm@mail.uc.edu>
2018-08-19 22:33:28 -0500

    added image and got a Name thing working

--- monopoly-js/src/App.js
+++ monopoly-js/src/App.js
@@ -1,36 +1,98 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import image from './monopoly_board.jpg';
 import './App.css';
 import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import { InputLabel } from '@material-ui/core';
+
+const PlayerNames = ({values}) => (
+  <div>
+    {values.map(function(currentValue, index) {
+      return(<div key={index}>
+        <div>
+          <div>Name: {currentValue.playerName}</div>  
+        </div>
+      </div>)
+    })}
+  </div>
+)
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state={
-      d1: 1,
-      d2: 1,
-      rollTotal: 2
+      dialogOpen: false,
+      playerName: '',
+      playerList: []
     };
 
-    this.handleRoll = this.handleRoll.bind(this);
+    this.handleClickOpen = this.handleClickOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleRoll(e) {
-    this.state.d1 = Math.floor((Math.random()*6) + 1);
-    this.state.d2 = Math.floor((Math.random()*6) + 1);
-    this.state.rollTotal = this.state.d1 + this.state.d2;
+  handleClickOpen = () => {
+    this.setState({
+      dialogOpen: true,
+    });
+  };
 
-    window.alert("Die 1: " + this.state.d1 + " and Die 2: " + this.state.d2 + " = " + this.state.rollTotal);
-    return false;
+  handleClose = () => {
+    this.setState({
+      dialogOpen: false
+    });
+  };
+
+  handleNameChange = e => {
+    this.setState({
+      playerName: e.target.value
+    });
+  };
+
+  handleSubmit = () => {
+    const newState = this.state.playerList;
+
+    newState.push({
+      playerName: this.state.playerName
+    });
+
+    this.setState({
+      dialogOpen: false,
+      playerName: '',
+      playerList: newState
+    });
   }
 
   render() {
     return (
       <div className="App">
-        <h1 className="App-title">Monopoly-JS</h1>
+        <header>
+          <img src={image} alt="Monopoly Board" />
+          <h1 className="App-title">Monopoly-JS</h1>
+        </header>
         <div>
-          <Button onClick={this.handleRoll}>Roll</Button>
+          <Button variant="outlined" onClick={this.handleClickOpen}>Start</Button>
+          <Dialog open={this.state.dialogOpen} onClose={this.handleClose}>
+            <div style={{padding: '50px'}}>
+              <FormControl>
+                <InputLabel id="name">Name</InputLabel>
+                <Input id="playerName" value={this.state.playerName} onChange={this.handleNameChange}></Input>
+              </FormControl>
+              <br />
+              <FormControl>
+                <Button variant="outlined" onClick={this.handleSubmit}>Submit</Button>
+              </FormControl>
+            </div>
+          </Dialog>
+        </div>
+        <div>
+          { ((this.state.playerList).length <= 0) ? <div /> : <PlayerNames values={this.state.playerList} /> }
         </div>
       </div>
     );
